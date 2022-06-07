41f37757bd7a46da661dc75e8ef7dd96da553341
kailash dhungana <dhunganak1920@gmail.com>
2022-06-07 16:28:24 +0545

    next version fixed empty valdation

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ let out = document.getElementById('result');
 
 submit.addEventListener('click',(e)=>{
     
+    if(p.value.length==0 || t.value.length==0 || r.value.length==0){
+        out.innerHTML = "Enter all inputs";
+    }
+    else{
+
         output = (p.value*t.value*r.value)/100;
-out.innerHTML = output;
+        out.innerHTML = output;
+    }
 
 e.preventDefault();
 });
